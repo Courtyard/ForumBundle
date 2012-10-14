de8188016ca7d7310725799c51980384626274b3
Adrian Schneider <siradrian@gmail.com>
2012-10-13 20:27:46 -0700

    edit post functionality

---
+++ Form/Type/Post/EditPostType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Courtyard\Bundle\ForumBundle\Form\Type\Post;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class EditPostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('message')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Courtyard\Bundle\ForumBundle\Entity\Post'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'forum_post_edit';
+    }
+}
